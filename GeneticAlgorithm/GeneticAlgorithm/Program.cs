a36945a88534e2531f110d03e733b537c096ee3a
Shahnamah <a.ghaffer5@gmail.com>
2018-04-17 13:39:58 +0200

    last change at genetic algorithm

--- GeneticAlgorithm/GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/GeneticAlgorithm/Program.cs
@@ -9,14 +9,6 @@ namespace GeneticAlgorithm
     {
         public static void Main(string[] args)
         {
-            /* FUNCTIONS TO DEFINE (for each problem):
-            Func<Ind> createIndividual;                                 ==> input is nothing, output is a new individual
-            Func<Ind,double> computeFitness;                            ==> input is one individual, output is its fitness
-            Func<Ind[],double[],Func<Tuple<Ind,Ind>>> selectTwoParents; ==> input is an array of individuals (population) and an array of corresponding fitnesses, output is a function which (without any input) returns a tuple with two individuals (parents)
-            Func<Tuple<Ind, Ind>, Tuple<Ind, Ind>> crossover;           ==> input is a tuple with two individuals (parents), output is a tuple with two individuals (offspring/children)
-            Func<Ind, double, Ind> mutation;                            ==> input is one individual and mutation rate, output is the mutated individual
-            */
-
             //Console.WriteLine("What is the crossoverRate");
             double crossoverRate = 0.85;
             //			double.TryParse (Console.ReadLine (), out crossoverRate);
@@ -33,18 +25,15 @@ namespace GeneticAlgorithm
             //				Convert.ToInt32(Console.ReadLine());
 
 
-            for (int i= 0; i< 100; i++)
-            {
-                String individual = CreateIndividual();
-                double fitness = ComputeFitness(individual);
-                GeneticAlgorithm<string> fakeProblemGA = new GeneticAlgorithm<string>(crossoverRate, mutationRate, elitism, populationSize, numIterations); // CHANGE THE GENERIC TYPE (NOW IT'S INT AS AN EXAMPLE) AND THE PARAMETERS VALUES
-                var solution = fakeProblemGA.Run(CreateIndividual, ComputeFitness, SelectTwoParents, Crossover, Mutation);
-                Console.WriteLine("Fitness: ");
-                Console.WriteLine(ComputeFitness(solution.Item1));
+            string individual = CreateIndividual();
+            double fitness = ComputeFitness(individual);
+            GeneticAlgorithm<string> fakeProblemGA = new GeneticAlgorithm<string>(crossoverRate, mutationRate, elitism, populationSize, numIterations); // CHANGE THE GENERIC TYPE (NOW IT'S INT AS AN EXAMPLE) AND THE PARAMETERS VALUES
+            var solution = fakeProblemGA.Run(CreateIndividual, ComputeFitness, SelectTwoParents, Crossover, Mutation);
+            Console.WriteLine("Fitness: ");
+            Console.WriteLine(ComputeFitness(solution.Item1));
 
-                Console.WriteLine("Solution: ");
-                Console.WriteLine(Convert.ToInt32(solution.Item1, 2));
-            }
+            Console.WriteLine("Solution: ");
+            Console.WriteLine(Convert.ToInt32(solution.Item1, 2));
 
 
             Console.Read();
